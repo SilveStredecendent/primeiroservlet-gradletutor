003bcff627a3d120148bebbb07abc565f5adfed1
SilveStredecendent <herbertdesousasantos31@gmail.com>
2023-05-03 17:47:17 +0000

    Salvar

---
+++ app/src/main/java/servlets/IndexServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "IndexServlet", urlPatterns= "/")
+public class IndexServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        // response.getWriter().println("Oi Servlet!!!");
+        // response.getWriter().close();
+        String nome = request.getParameter("nome");
+        request.setAttribute("nome", nome);
+        request.getRequestDispatcher("WEB-INF/home.jsp").forward(request, response);
+
+    }
+}
